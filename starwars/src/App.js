58d3cc3f0cce612bd9f6380da2e91b7abef4e7cd
Cameron Kirby <cameronevankirby@gmail.com>
2020-05-15 13:48:42 -0500

    Added MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'// Import Axios
+import styled from 'styled-components'; // Import styled-components
+
+import Character from './components/Character'
+
+import './App.css'
+
+const ContainerStyle = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  let [characters, setCharacters] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    // get the first set of characters
+    axios.get(`https://swapi.py4e.com/api/people/`)
+      .then(response => {
+          console.log(response.data.results)
+          setCharacters(response.data.results)
+      })
+      .catch(error => {})
+  }, []);
+
+  console.log(characters)
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <ContainerStyle>
+        {
+          characters.map((character, index) => {
+            return <Character key={index} character={character} />
+          })
+        }
+      </ContainerStyle>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,33 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'; // Import Axios
+import styled from 'styled-components'; // Import styled-components
+
+const CharacterStyle = styled.div`
+
+    .card {
+        padding: 5%;
+        background-color: white;
+        border-radius: 5px;
+    }
+`
+
+
+export default function Character(props){
+    const character = props.character
+
+    return(
+        <CharacterStyle>
+            <div className="card">
+                <h2>Name: {character.name}</h2>
+                <ul>
+                    <li>Birth Year: {character.birth_year}</li>
+                    <li>Gender: {character.gender}</li>
+                    <li>Hair Color: {character.hair_color}</li>
+                    <li>Weight: {character.mass}kg</li>
+                    <li>Eye color: {character.eye_color}</li>
+                </ul>
+            </div>
+        </CharacterStyle>
+    )
+}
